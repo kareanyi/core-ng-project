a15447d9f5c91c4787abb0e2579af2f32509e89e
neo <neowu.us@gmail.com>
2016-03-16 16:34:01 -0400

    create empty product

--- mongo-perf-test/src/main/java/service/MongoLoadTest.java
+++ mongo-perf-test/src/main/java/service/MongoLoadTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import core.framework.api.async.Executor;
 import core.framework.api.mongo.MongoCollection;
 import domain.Product;
 import domain.Sku;
@@ -7,6 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author neo
@@ -18,9 +23,26 @@ public class MongoLoadTest {
     MongoCollection<Product> productCollection;
 
     @Inject
+    Executor executor;
+
+    @Inject
     MongoCollection<Sku> skuCollection;
 
-    public void execute() {
-        logger.info("start load test");
+    public void execute() throws ExecutionException, InterruptedException {
+        logger.info("create empty product");
+        int size = 100000;
+        List<Future<Void>> futures = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            final int finalI = i;
+            futures.add(executor.submit("insert", () -> {
+                Product entity = new Product();
+                entity.id = "neo-test-" + finalI;
+                productCollection.replace(entity);
+                return null;
+            }));
+        }
+        for (Future<Void> future : futures) {
+            future.get();
+        }
     }
 }
