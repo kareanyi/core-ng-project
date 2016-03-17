df9a0e4905ea1d3be5371ffd55ef003df260fcaa
neo <neowu.us@gmail.com>
2016-03-17 15:32:19 -0400

    update test

--- mongo-perf-test/src/main/java/Main.java
+++ mongo-perf-test/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import core.framework.api.mongo.Mongo;
+import core.framework.impl.mongo.MongoImpl;
 import service.MongoLoadTest;
 
 import java.util.concurrent.ExecutionException;
@@ -9,6 +11,9 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MongoPerfTestApp app = new MongoPerfTestApp();
         app.configure();
+
+        MongoImpl mongo = (MongoImpl) app.bean(Mongo.class);
+        mongo.initialize();
 
 //        MongoPreWarmer preWarm = app.bean(MongoPreWarmer.class);
 //        preWarm.execute();
